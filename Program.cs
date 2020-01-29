38c82c3d2f2c8aef49de8aa14cd66f942429f5ea
Brennan Harrison <brennan16@usf.edu>
2020-01-29 18:47:10 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClassProjectIterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer between 1 and 100!");
+
+            try
+            {
+                int x = 1;
+
+                string input = Console.ReadLine();
+
+
+                int number = Int32.Parse(input);
+                
+                if (number < 1 || number > 100)
+                {
+                    Console.WriteLine("You must enter a number between 1 and 100!");
+                }
+                else
+                {
+                    while (x <= number)
+                    {
+                        Console.WriteLine("You have entered " + number.ToString() + "."
+                                            + " This is the current integer value in the loop: " + x.ToString());
+                        x += 1;
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("You must enter an INTEGER!");
+            }
+        }
+    }
+}
